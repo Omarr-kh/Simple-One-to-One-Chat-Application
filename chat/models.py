4c86fbf54ab6a39f498d815ab9b8746a2399c219
Omar <omarkhaled1752003@yahoo.com>
2024-09-03 20:40:46 +0300

    created models for ChatRoom, and Message

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="chatroom_sender",
+        null=True,
+        blank=True,
+    )
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="chatroom_receiver",
+        null=True,
+        blank=True,
+    )
+    last_message = models.TextField(null=True, blank=True)
+    last_message_date = models.DateTimeField(auto_now_add=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    chatroom = models.ForeignKey(
+        ChatRoom, on_delete=models.CASCADE, related_name="messages"
+    )
+    content = models.TextField(null=True, blank=True)
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_messages"
+    )
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
