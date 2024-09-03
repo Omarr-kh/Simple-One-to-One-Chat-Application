31e83533ebc2e06753545f04b7feb7b5344396a7
Omar <omarkhaled1752003@yahoo.com>
2024-09-03 22:52:25 +0300

    url route for chat consumer

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
-from . import consumers
+from django.urls import path
+from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/socket-server/", consumers.ChatConsumer.as_asgi())
+    path("ws/chatrooms/<int:chat_room_id>/", ChatConsumer.as_asgi())
 ]
