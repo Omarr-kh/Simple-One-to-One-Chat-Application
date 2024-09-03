b803385017a308d96c3015b91d49fc06793a2511
Omar <omarkhaled1752003@yahoo.com>
2024-09-03 22:52:48 +0300

    view for Chatroom creation

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import generics, permissions, status
+
+from .serializers import ChatRoomSerializer, MessageSerializer
+from .models import ChatRoom, Message
+
+
+class CreateChatroomView(generics.CreateAPIView):
+    serializer_class = ChatRoomSerializer
