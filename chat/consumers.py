a59df0b758021649189e67b8fda3a974af688ce0
Omar <omarkhaled1752003@yahoo.com>
2024-09-04 12:36:12 +0300

    pagination added

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,8 +1,30 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import ChatRoom, Message
-from django.contrib.auth.models import User
 from channels.db import database_sync_to_async
+from asgiref.sync import sync_to_async
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from .models import ChatRoom, Message
+from .serializers import MessageSerializer
+
+
+@sync_to_async
+def get_user(token):
+    return Token.objects.get(key=token).user
+
+
+@sync_to_async
+def last_10_message(chatroom):
+    try:
+        chatroom = ChatRoom.objects.get(id=chatroom)
+        messages = chatroom.messages.all().order_by("-created_at")[:10]
+        if messages.count() > 0:
+            messages_data = MessageSerializer(messages, many=True).data
+            return messages_data
+        else:
+            return {}
+    except ChatRoom.DoesNotExist:
+        return None
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -14,6 +36,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
+
+        messages = await last_10_message(self.room_id)
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "type": "last_50_messages",
+                    "messages": messages,
+                }
+            )
+        )
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
@@ -51,15 +83,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # Get the chatroom instance
         chatroom = ChatRoom.objects.get(id=self.room_id)
 
-        user = User.objects.create_user(username="test1", password="test12345")
-        user2 = User.objects.create_user(username="test2", password="test12345")
+        # user = self.user
+        # user2 = User.objects.create_user(username="test9", password="test12345")
 
         message = Message.objects.create(
             chatroom=chatroom,
             content=message_content,
-            sender=user,
-            receiver=user2,
-            # receiver=chatroom.receiver if chatroom.sender == user else chatroom.sender,
+            sender=self.user,
+            # receiver=user2,
+            receiver=(
+                chatroom.receiver if chatroom.sender == self.user else chatroom.sender
+            ),
         )
 
         chatroom.last_message = message_content
